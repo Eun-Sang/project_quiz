f04d40ed17a6f05c96171375a1ff5a189e792bf9
301-05\User <2420205@naver.com>
2022-03-17 21:45:52 +0900

    03-17

---
+++ src/main/java/com/SNS/post/PostController.java
@@ -0,0 +1,25 @@
+package com.SNS.post;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.SNS.post.bo.PostBO;
+import com.SNS.post.model.Post;
+
+@RequestMapping("/post")
+@Controller
+public class PostController {
+	
+	@Autowired
+	private PostBO postBO;
+	@RequestMapping("/post_list_view")
+	public String postListview(Model model) {
+		List<Post> postList = postBO.getPostList();
+		model.addAttribute("postList", postList);
+		return "test/quiz";
+	}
+}
---
+++ src/main/java/com/SNS/post/bo/PostBO.java
@@ -0,0 +1,20 @@
+package com.SNS.post.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.SNS.post.dao.PostDAO;
+import com.SNS.post.model.Post;
+
+@Service
+public class PostBO {
+
+	@Autowired
+	private PostDAO postDAO;
+	
+	public List<Post> getPostList() {
+		return postDAO.selectPostList();
+	}
+}
